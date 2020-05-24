f0f75b8cd47ac289ddff6679bf32e91dd110f43b
lozunoff <andrey.lozunov@gmail.com>
2020-05-24 10:43:47 +0400

    Добавлена клиентская часть (верстка, скрипты)

---
+++ public/app.js
@@ -0,0 +1,140 @@
+// eslint-disable-next-line no-new, no-undef
+new Vue({
+  el: '#app',
+  data() {
+    return {
+      isDark: true,
+      show: true,
+      todoTitle: '',
+      todos: [],
+    };
+  },
+  created() {
+    const query = `
+      query {
+        getTodos {
+          id, title, done, createdAt, updatedAt
+        }
+      }
+    `;
+
+    fetch('/graphql', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        this.todos = response.data.getTodos;
+      });
+  },
+  methods: {
+    addTodo() {
+      const title = this.todoTitle.trim();
+      if (!title) {
+        return;
+      }
+
+      const query = `
+        mutation {
+          createTodo(todo: {title: "${title}"}) {
+            id, title, done, createdAt, updatedAt
+          }
+        }
+      `;
+
+      fetch('/graphql', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          const todo = response.data.createTodo;
+          this.todos.push(todo);
+          this.todoTitle = '';
+        })
+        .catch((e) => {
+          // eslint-disable-next-line no-console
+          console.log(e);
+        });
+    },
+    removeTodo(id) {
+      const query = `
+        mutation {
+          deleteTodo(id: "${id}")
+        }
+      `;
+
+      fetch('/graphql', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      })
+        .then(() => {
+          // eslint-disable-next-line arrow-parens
+          this.todos = this.todos.filter(t => t.id !== id);
+        })
+        .catch((e) => {
+          // eslint-disable-next-line no-console
+          console.log(e);
+        });
+    },
+    completeTodo(id) {
+      const query = `
+        mutation {
+          completeTodo(id: "${id}") {
+            updatedAt
+          }
+        }
+      `;
+
+      fetch('/graphql', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ query }),
+      })
+        .then((res) => res.json())
+        .then((response) => {
+          const idx = this.todos.findIndex((t) => t.id === id);
+          this.todos[idx].updatedAt = response.data.completeTodo.updatedAt;
+        })
+        .catch((e) => {
+          // eslint-disable-next-line no-console
+          console.log(e);
+        });
+    },
+  },
+  filters: {
+    capitalize(value) {
+      return value.toString().charAt(0).toUpperCase() + value.slice(1);
+    },
+    date(value, withTime) {
+      const options = {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+      };
+
+      if (withTime) {
+        options.hour = '2-digit';
+        options.minute = '2-digit';
+        options.second = '2-digit';
+      }
+
+      return new Intl.DateTimeFormat('ru-RU', options).format(new Date(+value));
+    },
+  },
+});
